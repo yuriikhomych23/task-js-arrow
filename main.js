9617229e56379e7cb51c72ce0a3a959787dbb8e9
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-19 16:21:48 +0000

    Sample code

---
+++ main.js
@@ -0,0 +1,30 @@
+//TASK 1
+
+// let fab = a => {
+//     return (a >= 1) ? a : fab(a - 1) + fab (a - 2);
+// }
+
+// console.log( fab (20) )
+
+
+// TASK 2
+
+// let ask1 = +prompt('Введіть число "x" ?')
+// let ask2 = +prompt('Введіть число "y" ?');
+
+// let simpleNumbers = (x, y) => {
+//     for(let i = x; i <= y; i++) {
+//         let j;
+//         for(j = 2; j < y; j++) {
+//             if(i % j == 0) {
+//                 break;
+//             }
+//         } if(i == j) console.log(i)
+//     }
+// }
+
+// simpleNumbers(ask1, ask2);
+
+
+
+ 
